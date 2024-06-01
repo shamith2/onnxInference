7cfde66c523b08f2d5e6825e86344ffc61665f91
Shamith Achanta <43729418+shamith2@users.noreply.github.com>
2024-05-31 23:10:27 -0500

    Update onnx_profiling.py

--- scripts/onnxProfile/onnx_profiling.py
+++ scripts/onnxProfile/onnx_profiling.py
@@ -1,7 +1,10 @@
 # Script for profiling onnx model
 
 import os
+import sys
+
 import onnx
+import onnxruntime
 
 from pathlib import Path
 
@@ -13,7 +16,25 @@ onnx_t = ONNXTransformer()
 
 uninferred_onnx_model_path = os.path.join(workspace, 'models', 'model.onnx')
 
-inferred_onnx_model_path = onnx_t.shapeInfer('sd_unet', uninferred_onnx_model_path, [(1, 4, 64, 64), (1,), (1, 77, 1024)], [(1, 4, 64, 64)])
+def get_shapes():
+    dummy_session = onnxruntime.InferenceSession(
+                    uninferred_onnx_model_path,
+                    providers=["CPUExecutionProvider"]
+                )
+    
+    print("Inputs: ")
+    for _input in dummy_session.get_inputs():
+        print(_input.name, _input.shape, _input.type)
+    
+    print("Outputs: ")
+    for output in dummy_session.get_outputs():
+        print(output.name, output.shape, output.type)
+    
+    sys.exit()
+
+# get_shapes()
+
+inferred_onnx_model_path = onnx_t.shapeInfer('sdxlt_unet', uninferred_onnx_model_path, [(1, 4, 64, 64), (1,), (1, 77, 2048), (1, 1280), (1, 6)], [(1, 4, 64, 64)])
 
 onnx_t.profileMemory(inferred_onnx_model_path)
 
