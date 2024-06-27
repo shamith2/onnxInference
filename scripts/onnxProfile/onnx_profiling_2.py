a6395cab00a9eee4ebdc95c8a2f5539741b3f45c
Shamith Achanta <achantashamith007@gmail.com>
2024-06-27 18:02:56 -0500

    added files

---
+++ scripts/onnxProfile/onnx_profiling_2.py
@@ -0,0 +1,72 @@
+# Script for profiling onnx model
+
+import os
+import sys
+
+import onnx
+import onnxruntime
+
+from pathlib import Path
+
+from onnxInsights.onnxHelpers import ONNXTransformer
+
+workspace = Path(__file__).parent.resolve()
+
+onnx_t = ONNXTransformer()
+
+uninferred_onnx_model_path = os.path.join(workspace, 'models', 'model.onnx')
+
+def get_shapes(model_path):
+    dummy_session = onnxruntime.InferenceSession(
+                    model_path,
+                    providers=["CPUExecutionProvider"]
+                )
+    
+    print("Inputs: ")
+    for _input in dummy_session.get_inputs():
+        print(_input.name, _input.shape, _input.type)
+    
+    print("Outputs: ")
+    for output in dummy_session.get_outputs():
+        print(output.name, output.shape, output.type)
+    
+    sys.exit()
+
+
+# get_shapes(uninferred_onnx_model_path)
+
+# inferred_onnx_model_path = onnx_t.shapeInfer('sdxlt_unet', uninferred_onnx_model_path, [(1, 4, 64, 64), (1,), (1, 77, 2048), (1, 1280), (1, 6)], [(1, 4, 64, 64)])
+
+# for llm: tiny llama
+PHASE = 'PROMPT'
+BATCH_SIZE = 1
+SEQ_LEN = 1024 if PHASE == 'PROMPT' else 1
+MAX_LEN = 2048
+CACHE_LEN = 1 if PHASE == 'PROMPT' else MAX_LEN - 1
+
+uninferred_llm_onnx_model_path = os.path.join(workspace, 'models', 'rank_0_Meta-Llama-3-8B-Instruct_decoder_merged_model_fp16.onnx')
+
+input_shapes = [(BATCH_SIZE, SEQ_LEN), (BATCH_SIZE, MAX_LEN), (BATCH_SIZE, SEQ_LEN)]
+
+for i in range(32):
+    input_shapes.append((BATCH_SIZE, 8, CACHE_LEN, 128)) # for key
+    input_shapes.append((BATCH_SIZE, 8, CACHE_LEN, 128)) # for value
+
+output_shapes = [(BATCH_SIZE, SEQ_LEN, 128256)]
+
+for i in range(32):
+    output_shapes.append((BATCH_SIZE, 8, MAX_LEN, 128)) # for key
+    output_shapes.append((BATCH_SIZE, 8, MAX_LEN, 128)) # for value
+
+inferred_onnx_model_path = onnx_t.shapeInfer(
+    'llama3_8b_fp16_' + PHASE.lower() + 'Phase',
+    uninferred_llm_onnx_model_path,
+    input_shapes,
+    output_shapes
+)
+
+onnx_t.profileMemory(inferred_onnx_model_path)
+
+# onnx_t.profileModel(inferred_onnx_model_path)
+
+# onnx_t.modifyGraph(delete_block=['DequantizeLinear', 'Clip', 'QuantizeLinear'], upper_2_ok=False, only_middle=True)
